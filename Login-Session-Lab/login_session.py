7ecec9ceeaf7d30c783ec58fe69efefa46411422
lianfa23-meet <109899264+lianfa23-meet@users.noreply.github.com>
2022-07-28 15:19:48 +0300

    completed without bonus

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask import session as login_session
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/', methods=['GET', 'POST']) # What methods are needed?
 def home():
-	
-	return render_template('home.html')
+	if request.method == 'POST':
+		try:
+			author = request.form['Author']
+			age = request.form['Age']
+			quote = request.form['Quote']
+			login_session['author'] = author
+			login_session['age'] = age
+			login_session['quote'] = quote
+			return render_template('thanks.html', quote=quote, author=author, age=age)
 
+		except:
+			return render_template('error.html')
+
+
+	else:
+		return render_template('home.html')
 
 @app.route('/error')
 def error():
@@ -19,7 +33,7 @@ def error():
 @app.route('/display')
 def display():
 
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html', quote=login_session['quote'], name=login_session['author'], age=login_session['age']) # What variables are needed?
 
 
 @app.route('/thanks')
